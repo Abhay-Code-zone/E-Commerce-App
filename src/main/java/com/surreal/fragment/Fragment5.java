6da95657ede07455bb7b5dd96fe3ba4365f88c6f
Abhay-Code-Zone <abhay@example.com>
2023-12-12 12:04:00 +0530

    feat: Time folded in on itself

---
+++ src/main/java/com/surreal/fragment/Fragment5.java
@@ -0,0 +1,13 @@
+package com.surreal.fragment;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Fragment5 {
+    
+    private final String narrative = "Time folded in on itself";
+    
+    public String getNarrative() {
+        return narrative;
+    }
+}
